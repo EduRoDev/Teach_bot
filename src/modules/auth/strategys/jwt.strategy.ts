f826cb93e911de2c55130fbd6cf5fb94876e37e6
EduRoDev <solanoe934@gmail.com>
2026-04-19 13:04:58 -0500

    feat: simplificar estrategia JWT eliminando manejo de sesiones y validación de expiración

--- src/modules/auth/strategys/jwt.strategy.ts
+++ src/modules/auth/strategys/jwt.strategy.ts
@@ -1,33 +1,20 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { UserRoleEnum } from '@prisma/client';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { envs } from 'src/config';
-import { SessionsService } from '../sessions/sessions.service';
-import { Request } from 'express';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
-    constructor(
-        private sessions: SessionsService,
-    ) {
+    constructor() {
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
             ignoreExpiration: false,
             secretOrKey: envs.JWT_SECRET,
-            passReqToCallback: true,
         });
     }
 
-    async validate(payload: { sub: number; role: UserRoleEnum }, req: Request) {
-        const sessionId = req.cookies?.sessionId;
-        if (!sessionId) throw new UnauthorizedException('Session ID not found in cookies');
-
-        const session = await this.sessions.findOne({ id: sessionId, userId: payload.sub });
-        if (!session.expiresAt || new Date(session.expiresAt) < new Date()) {
-            throw new UnauthorizedException('Session has expired');
-        }
-        return { ...payload, sessionId }
-
+    validate(payload: { sub: number; role: UserRoleEnum }) {
+        return { userId: payload.sub, role: payload.role };
     }
 }
